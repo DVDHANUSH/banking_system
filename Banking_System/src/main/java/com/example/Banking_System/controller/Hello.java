935adcb5932b8f34cb071c972d1e1671d5b4fadf
DV DHANUSH <ddomavenkata@red-shift.us>
2024-06-28 07:52:46 +0530

    Created a basic controller for checking whether the apis calls are working fine. Integrated mysql and swagger.

--- Banking_System/src/main/java/com/example/Banking_System/controller/Hello.java
+++ Banking_System/src/main/java/com/example/Banking_System/controller/Hello.java
@@ -5,6 +5,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class Hello {
     @GetMapping("sayHello")
     public String sayHello(){
-        return "Hello";
+        return "Hello there pppp";
     }
 }
